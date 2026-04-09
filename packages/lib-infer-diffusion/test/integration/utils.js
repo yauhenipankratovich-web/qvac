0231828218017d9f65a27a7ec6dc36149f0628af
Maksim Smatrou <maxim.smotrov@itrexgroup.com>
2026-04-09 11:43:58 +0200

    Changing generated images dir on iOS #3

--- packages/lib-infer-diffusion/test/integration/utils.js
+++ packages/lib-infer-diffusion/test/integration/utils.js
@@ -3,6 +3,7 @@ const fs = require('bare-fs')
 const path = require('bare-path')
 const https = require('bare-https')
 const os = require('bare-os')
+const proc = require('bare-process')
 
 async function downloadFile (url, dest) {
   return new Promise((resolve, reject) => {
@@ -169,9 +170,21 @@ function setupJsLogger (binding) {
   return binding
 }
 
+function getIosArtifactDir () {
+  const homeDir = proc.env?.HOME_DIR || proc.argv?.[0]
+  if (!homeDir || homeDir === 'worker.js' || homeDir.startsWith('file://')) {
+    throw new Error(`Could not resolve writable iOS Documents directory from runtime: ${homeDir || 'missing'}`)
+  }
+  return path.join(homeDir, 'test', 'generated-images')
+}
+
 function saveGeneratedImageArtifact(modelDir, filename, imageData) {
   if (os.platform() !== 'android') {
-    const primaryOutPath = path.join(modelDir, filename)
+    const artifactDir = os.platform() === 'ios'
+      ? getIosArtifactDir()
+      : modelDir
+    fs.mkdirSync(artifactDir, { recursive: true })
+    const primaryOutPath = path.join(artifactDir, filename)
     fs.writeFileSync(primaryOutPath, imageData)
     console.log(`\nImage saved to ${primaryOutPath}`)
     return
